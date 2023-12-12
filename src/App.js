47e699b624419ceeafe7bc33660ded5d3c5a3101
Charlie Gray <charliechasegray@gmail.com>
2023-12-11 16:39:59 -0800

    final commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
 
-function App() {
+export default function App() {
+  const [isHidden, setIsHidden] = useState(false);
+
+  function handleClick(event) {
+    setIsHidden(!isHidden);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={handleClick}>{isHidden ? "Unhide" : "Hide"}</button>
+      {isHidden ? null : (
+        <div className="App">
+          <h1>Hello CodeSandbox</h1>
+          <h2>Start editing to see some magic happen!</h2>
+        </div>
+      )}
     </div>
   );
 }
-
-export default App;
